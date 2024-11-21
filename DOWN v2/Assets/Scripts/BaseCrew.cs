ba714c9199bc264861c93f6bd318d642fd79706d
jacksonhitz <jacksonhitz@gmail.com>
2024-11-20 21:50:31 -0800

    stuff

--- DOWN v2/Assets/Scripts/BaseCrew.cs
+++ DOWN v2/Assets/Scripts/BaseCrew.cs
@@ -3,261 +3,185 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-
 public class BaseCrew : MonoBehaviour
 {
-   public bool selected;
-   public Pathfinder pathfinder;
-   public List<Tile> path = new List<Tile>();
-   public Tile startTile;
-   public Tile endTile;
-   [SerializeField] private float speed;
-   private GridManager gridManager;
-   private GameObject highlight;
-   private Animator animator;
-   public int health;
+    public bool selected;
+    public Pathfinder pathfinder;
+    public List<Tile> path = new List<Tile>();
+    public Tile startTile;
+    public Tile endTile;
+    [SerializeField] private float speed;
+    private GameObject highlight;
+    private Animator animator;
+    public int health;
 
     public bool fixing;
 
-   [SerializeField] Slider healthBar;
-   [SerializeField] Slider progressBar;
-   [SerializeField] float fillSpeed = 0.001f;
-   float targetProgress = 1f;
+    [SerializeField] Slider healthBar;
+    [SerializeField] Slider progressBar;
+    [SerializeField] float fillSpeed = 0.001f;
+    float targetProgress = 1f;
 
+    GridManager gridManager;
 
-   // Start is called before the first frame update
-   public void Start()
-   {
-       gridManager = FindObjectOfType<GridManager>();
-       startTile = transform.parent.GetComponent<Tile>();
-       startTile.isOccupied = true;
-       pathfinder = new Pathfinder();
-       gridManager.crew.Add(this);
-       animator = GetComponent<Animator>();
-       highlight = transform.Find("Highlight").gameObject;
+    public float damageInterval = 1f; // How often damage is applied (in seconds)
+    private float nextDamageTime;
 
+    public void Start()
+    {
+        startTile = transform.parent.GetComponent<Tile>();
+        startTile.isOccupied = true;
+        pathfinder = new Pathfinder();
+        GridManager.Instance.crew.Add(this);
+        animator = GetComponent<Animator>();
+        highlight = transform.Find("Highlight").gameObject;
 
-       StationCheck();
-   }
+        gridManager = FindObjectOfType<GridManager>();
 
+        StationCheck();
+        nextDamageTime = Time.time; // Initialize the damage timer
+    }
 
+    void Update()
+    {
+        if (path.Count > 0 && selected)
+        {
+            MoveAlongPath();
+            Vector2 currentPosition = transform.position;
+            Vector2 targetPosition = path[0].transform.position;
+            Vector2 movement = (targetPosition - currentPosition).normalized;
+            animator.SetBool("Moving", true);
+            animator.SetFloat("Horizontal", movement.x);
+            animator.SetFloat("Vertical", movement.y);
+        }
+        else
+        {
+            animator.SetBool("Moving", false);
+        }
 
+        highlight.SetActive(selected);
 
-   void Update()
-   {
-       if (path.Count > 0 && selected)
-       {
-           MoveAlongPath();
-           Vector2 currentPosition = transform.position;
-           Vector2 targetPosition = path[0].transform.position;
-           Vector2 movement = (targetPosition - currentPosition).normalized;
-           animator.SetBool("Moving", true);
-           animator.SetFloat("Horizontal", movement.x);
-           animator.SetFloat("Vertical", movement.y);
-       }
-       else
-       {
-           animator.SetBool("Moving", false);
-       }
+        path = pathfinder.FindPath(startTile, endTile);
 
-
-       if (selected)
-       {
-           highlight.SetActive(true);
-       }
-       else
-       {
-           highlight.SetActive(false);
-       }
-
-
-       path = pathfinder.FindPath(startTile, endTile);
-
-
-       if (startTile == endTile)
-       {
-           endTile = null;
-       }
-
-       //EnemyCheck();
-
+        if (startTile == endTile)
+        {
+            endTile = null;
+        }
 
         if (fixing)
         {
             progressBar.gameObject.SetActive(true);
-            
+
             if (progressBar.value < targetProgress)
             {
                 progressBar.value += fillSpeed * Time.deltaTime;
             }
             else
             {
-                fixing = false; 
-                progressBar.value = 0f; 
-                progressBar.gameObject.SetActive(false); 
+                fixing = false;
+                progressBar.value = 0f;
+                progressBar.gameObject.SetActive(false);
                 gridManager.Patched(startTile);
                 animator.SetBool("Moving", false);
                 animator.Play("CrewIdle");
             }
         }
-   }
 
+        // Apply damage based on pressure if standing on a tile with pressure
+        ApplyPressureDamage();
+    }
 
-   public void OnMouseDown()
-   {
-       foreach (BaseCrew crew in gridManager.crew)
-       {
-           crew.selected = false;
-       }
-       selected = true;
-       gridManager.moveable = true;
-   }
+    public void OnMouseDown()
+    {
+        foreach (BaseCrew crew in GridManager.Instance.crew)
+        {
+            crew.selected = false;
+        }
+        selected = true;
+    }
 
+    public void MoveAlongPath()
+    {
+        transform.position = Vector2.MoveTowards(transform.position, path[0].transform.position, speed * Time.deltaTime);
 
-   public void MoveAlongPath()
-   {
-       transform.position = Vector2.MoveTowards(transform.position, path[0].transform.position, speed * Time.deltaTime);
+        if (Vector2.Distance(transform.position, path[0].transform.position) < 0.00001f)
+        {
+            FinishedMoving(path[0]);
+        }
+    }
 
+    public void FinishedMoving(Tile tile)
+    {
+        startTile = tile;
+        transform.parent = tile.transform;
 
-       if (Vector2.Distance(transform.position, path[0].transform.position) < 0.00001f)
-       {
-           FinishedMoving(path[0]);
-           //path.RemoveAt(0);
-       }
-   }
+        StationCheck();
+        BreachCheck();
+    }
 
+    void BreachCheck()
+    {
+        if (startTile.CompareTag("Breach"))
+        {
+            animator.SetTrigger("DStationIdle");
+            fixing = true;
+        }
+    }
 
+    void StationCheck()
+    {
+        Vector2Int crewPosition = startTile.gridLocation;
 
+        Vector2Int[] adjacentTileOffsets =
+        {
+            new Vector2Int(0, 1),   // Above
+            new Vector2Int(0, -1),  // Below
+            new Vector2Int(-1, 0),  // Left
+            new Vector2Int(1, 0)    // Right
+        };
 
-   public void FinishedMoving(Tile tile)
-   {
-       startTile = tile;
-       transform.parent = tile.transform;
+        if (CheckStationTiles(GridManager.Instance.uStationTiles, crewPosition, adjacentTileOffsets))
+        {
+            animator.SetTrigger("UStationIdle");
+        }
+        else if (CheckStationTiles(GridManager.Instance.dStationTiles, crewPosition, adjacentTileOffsets))
+        {
+            animator.SetTrigger("DStationIdle");
+        }
+        else if (CheckStationTiles(GridManager.Instance.lStationTiles, crewPosition, adjacentTileOffsets))
+        {
+            animator.SetTrigger("LStationIdle");
+        }
+        else if (CheckStationTiles(GridManager.Instance.rStationTiles, crewPosition, adjacentTileOffsets))
+        {
+            animator.SetTrigger("RStationIdle");
+        }
+    }
 
-       StationCheck();
-       BreachCheck();
-   }
+    private bool CheckStationTiles(List<Vector2Int> stationTiles, Vector2Int crewPosition, Vector2Int[] adjacentTileOffsets)
+    {
+        foreach (Vector2Int offset in adjacentTileOffsets)
+        {
+            Vector2Int adjacentTileLocation = crewPosition + offset;
 
-
-   void BreachCheck()
-   {
-       if (startTile.CompareTag("Breach"))
-       {
-           animator.SetTrigger("DStationIdle");
-           Debug.Log("On Breach");
-           fixing = true;
-       }
-   }
-
-
-   void StationCheck()
-   {
-       Vector2Int crewPosition = startTile.gridLocation;
-
-
-       Vector2Int[] adjacentTileOffsets =
-       {
-           new Vector2Int(0, 1),   // Above
-           new Vector2Int(0, -1),  // Below
-           new Vector2Int(-1, 0),  // Left
-           new Vector2Int(1, 0)    // Right
-       };
-
-
-       if (CheckStationTiles(GridManager.Instance.uStationTiles, crewPosition, adjacentTileOffsets))
-       {
-           animator.SetTrigger("UStationIdle");
-       }
-       else if (CheckStationTiles(GridManager.Instance.dStationTiles, crewPosition, adjacentTileOffsets))
-       {
-           animator.SetTrigger("DStationIdle");
-       }
-       else if (CheckStationTiles(GridManager.Instance.lStationTiles, crewPosition, adjacentTileOffsets))
-       {
-           animator.SetTrigger("LStationIdle");
-       }
-       else if (CheckStationTiles(GridManager.Instance.rStationTiles, crewPosition, adjacentTileOffsets))
-       {
-           animator.SetTrigger("RStationIdle");
-       }
-   }
-
-
-   void EnemyCheck()
-   {
-       Vector2Int crewPosition = startTile.gridLocation;
-
-
-       Vector2Int[] adjacentTileOffsets =
-       {
-           new Vector2Int(0, 1),   // Above
-           new Vector2Int(0, -1),  // Below
-           new Vector2Int(-1, 0),  // Left
-           new Vector2Int(1, 0)    // Right
-       };
-
-
-       if (CheckForEnemy(adjacentTileOffsets[0]))
-       {
-           animator.SetTrigger("UAttack");
-       }
-       else if (CheckForEnemy(adjacentTileOffsets[1]))
-       {
-           animator.SetTrigger("DAttack");
-       }
-       else if (CheckForEnemy(adjacentTileOffsets[2]))
-       {
-           animator.SetTrigger("LAttack");
-       }
-       else if (CheckForEnemy(adjacentTileOffsets[3]))
-       {
-           animator.SetTrigger("RAttack");
-       }
-   }
-
-
-
-
-   private bool CheckStationTiles(List<Vector2Int> stationTiles, Vector2Int crewPosition, Vector2Int[] adjacentTileOffsets)
-   {
-       foreach (Vector2Int offset in adjacentTileOffsets)
-       {
-           Vector2Int adjacentTileLocation = crewPosition + offset;
-
-
-           if (stationTiles.Contains(adjacentTileLocation))
-           {
-               return true;
-           }
-       }
-       return false;
-   }
-
-
-   private bool CheckForEnemy(Vector2Int offset)
-   {
-       Vector2Int adjacentTileLocation = startTile.gridLocation + offset;
-
-
-       if (GridManager.Instance.tiles.ContainsKey(adjacentTileLocation))
-       {
-           Tile adjacentTile = GridManager.Instance.tiles[adjacentTileLocation];
-           Transform[] childTransforms = adjacentTile.GetComponentsInChildren<Transform>();
-
-
-           foreach (Transform childTransform in childTransforms)
-           {
-               BaseEnemy baseEnemy = childTransform.GetComponent<BaseEnemy>();
-               baseEnemy.TakeDamage(10);
-               if (childTransform.CompareTag("Enemy"))
-               {
-                   return true;
-               }
-           }
-       }
-       return false;
-   }
+            if (stationTiles.Contains(adjacentTileLocation))
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+    void ApplyPressureDamage()
+    {
+        if (startTile != null && startTile.pressure > 0)
+        {
+            if (Time.time >= nextDamageTime)
+            {
+                nextDamageTime = Time.time + damageInterval; 
+                health -= startTile.pressure; 
+                health = Mathf.Max(health, 0); 
+                healthBar.value = (float)health / 100f; 
+            }
+        }
+    }
 }
-
-
-
--- DOWN v2/Assets/Scripts/GridManager.cs
+++ DOWN v2/Assets/Scripts/GridManager.cs
@@ -2,195 +2,195 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-
 public class GridManager : MonoBehaviour
 {
-   private static GridManager instance;
-   public static GridManager Instance { get { return instance; } }
+    private static GridManager instance;
+    public static GridManager Instance { get { return instance; } }
 
+    [SerializeField] private int width, height;
+    [SerializeField] private Tile floorPrefab;
+    [SerializeField] private Tile doorPrefab;
+    [SerializeField] private Tile stationPrefab;
+    [SerializeField] private GameObject pilCrew;
+    [SerializeField] private GameObject engCrew;
+    [SerializeField] private GameObject docCrew;
+    [SerializeField] private GameObject enemyPrefab;
 
-   [SerializeField] private int width, height;
-   [SerializeField] private Tile floorPrefab;
-   [SerializeField] private Tile doorPrefab;
-   [SerializeField] private Tile stationPrefab;
-   [SerializeField] private GameObject pilCrew;
-   [SerializeField] private GameObject engCrew;
-   [SerializeField] private GameObject docCrew;
-   [SerializeField] private GameObject enemyPrefab;
+    [SerializeField] private Sprite[] floorSprites;
+    [SerializeField] private Sprite breachSprite;
 
+    [SerializeField] private List<Vector2Int> nullTiles;
+    [SerializeField] private List<Vector2Int> hDoorTiles;
+    [SerializeField] private List<Vector2Int> vDoorTiles;
+    [SerializeField] private List<Vector2Int> floorTiles;
 
-   [SerializeField] private Sprite[] floorSprites;
-   [SerializeField] private Sprite breachSprite;
+    public List<Vector2Int> uStationTiles;
+    public List<Vector2Int> dStationTiles;
+    public List<Vector2Int> lStationTiles;
+    public List<Vector2Int> rStationTiles;
 
+    public List<BaseCrew> crew = new List<BaseCrew>();
+    public Dictionary<Vector2, Tile> tiles;
 
-   [SerializeField] private List<Vector2Int> nullTiles;
-   [SerializeField] private List<Vector2Int> hDoorTiles;
-   [SerializeField] private List<Vector2Int> vDoorTiles;
-   [SerializeField] private List<Vector2Int> floorTiles;
+    private Transform pilSpawn;
+    private Transform engSpawn;
+    private Transform docSpawn;
+    private Transform enemySpawn;
 
+    private List<Vector2Int> breachTiles = new List<Vector2Int>();
 
-   public List<Vector2Int> uStationTiles;
-   public List<Vector2Int> dStationTiles;
-   public List<Vector2Int> lStationTiles;
-   public List<Vector2Int> rStationTiles;
+    void Start()
+    {
+        instance = this;
+        GenerateGrid();
 
+        pilSpawn = tiles[new Vector2(7, 2)].transform;
+        Instantiate(pilCrew, pilSpawn);
+        engSpawn = tiles[new Vector2(2, 6)].transform;
+        Instantiate(engCrew, engSpawn);
+        docSpawn = tiles[new Vector2(11, 7)].transform;
+        Instantiate(docCrew, docSpawn);
 
-   public List<BaseCrew> crew = new List<BaseCrew>();
+        StartCoroutine(BreachRoutine());
+    }
 
+    IEnumerator BreachRoutine()
+    {
+        yield return new WaitForSeconds(5f);
 
-   public Dictionary<Vector2, Tile> tiles;
+        while (true)
+        {
+            GenerateBreach();
+            yield return new WaitForSeconds(5f);
+        }
+    }
 
+    void GenerateGrid()
+    {
+        tiles = new Dictionary<Vector2, Tile>();
+        floorTiles = new List<Vector2Int>();
 
-   private Transform pilSpawn;
-   private Transform engSpawn;
-   private Transform docSpawn;
+        for (int x = 0; x < width; x++)
+        {
+            for (int y = 0; y < height; y++)
+            {
+                var tileLocation = new Vector2Int(x, y);
+                Tile spawnedTile;
 
+                if (nullTiles.Contains(tileLocation))
+                {
+                    spawnedTile = null;
+                }
+                else if (vDoorTiles.Contains(tileLocation))
+                {
+                    spawnedTile = Instantiate(doorPrefab, new Vector3(x, y), Quaternion.Euler(0f, 0f, 90f));
+                }
+                else if (hDoorTiles.Contains(tileLocation))
+                {
+                    spawnedTile = Instantiate(doorPrefab, new Vector3(x, y), Quaternion.identity);
+                }
+                else if (uStationTiles.Contains(tileLocation))
+                {
+                    spawnedTile = Instantiate(stationPrefab, new Vector3(x, y), Quaternion.identity);
+                }
+                else if (dStationTiles.Contains(tileLocation))
+                {
+                    spawnedTile = Instantiate(stationPrefab, new Vector3(x, y), Quaternion.Euler(0f, 0f, 180f));
+                }
+                else if (lStationTiles.Contains(tileLocation))
+                {
+                    spawnedTile = Instantiate(stationPrefab, new Vector3(x, y), Quaternion.Euler(0f, 0f, 90f));
+                }
+                else if (rStationTiles.Contains(tileLocation))
+                {
+                    spawnedTile = Instantiate(stationPrefab, new Vector3(x, y), Quaternion.Euler(0f, 0f, 270f));
+                }
+                else
+                {
+                    Sprite randomSprite = floorSprites[Random.Range(0, floorSprites.Length)];
+                    spawnedTile = Instantiate(floorPrefab, new Vector3(x, y), Quaternion.identity);
+                    spawnedTile.GetComponent<SpriteRenderer>().sprite = randomSprite;
+                    floorTiles.Add(tileLocation);
+                }
 
-   private Transform enemySpawn;
+                if (spawnedTile != null)
+                {
+                    spawnedTile.name = $"Tile {x} {y}";
+                    tiles[new Vector2(x, y)] = spawnedTile;
+                    spawnedTile.gridLocation = tileLocation;
+                }
+            }
+        }
+    }
 
-
-   public bool moveable;
-
-
-   void Start()
-   {
-       instance = this;
-      
-       GenerateGrid();
-
-
-       pilSpawn = tiles[new Vector2(7, 2)].transform;
-       Instantiate(pilCrew, pilSpawn);
-       engSpawn = tiles[new Vector2(2, 6)].transform;
-       Instantiate(engCrew, engSpawn);
-       docSpawn = tiles[new Vector2(11, 7)].transform;
-       Instantiate(docCrew, docSpawn);
-
-
-       StartCoroutine(BreachRoutine());
-   }
-
-
-   IEnumerator BreachRoutine()
-   {
-       while (true)
-       {
-           GenerateBreach();
-           yield return new WaitForSeconds(5f);
-       }
-   }
-
-
-   IEnumerator SpawnEnemies(float interval)
-   {
-       while (true)
-       {
-           yield return new WaitForSeconds(interval);
-
-
-           List<Tile> unoccupiedTiles = new List<Tile>();
-           foreach (var tile in tiles.Values)
-           {
-               if (!tile.isOccupied)
-               {
-                   unoccupiedTiles.Add(tile);
-               }
-           }
-
-
-           if (unoccupiedTiles.Count > 0)
-           {
-               Tile randomTile = unoccupiedTiles[Random.Range(0, unoccupiedTiles.Count)];
-               enemySpawn = tiles[new Vector2(randomTile.gridLocation.x, randomTile.gridLocation.y)].transform;
-               Instantiate(enemyPrefab, enemySpawn);
-           }
-       }
-   }
-
-
-   void GenerateGrid()
-   {
-       tiles = new Dictionary<Vector2, Tile>();
-       floorTiles = new List<Vector2Int>();
-
-
-       for (int x = 0; x < width; x++)
-       {
-           for (int y = 0; y < height; y++)
-           {
-               var tileLocation = new Vector2Int(x, y);
-               Tile spawnedTile;
-
-
-               if (nullTiles.Contains(tileLocation))
-               {
-                   spawnedTile = null;
-               }
-               else if (vDoorTiles.Contains(tileLocation))
-               {
-                   spawnedTile = Instantiate(doorPrefab, new Vector3(x, y), Quaternion.Euler(0f, 0f, 90f));
-               }
-               else if (hDoorTiles.Contains(tileLocation))
-               {
-                   spawnedTile = Instantiate(doorPrefab, new Vector3(x, y), Quaternion.identity);
-               }
-               else if (uStationTiles.Contains(tileLocation))
-               {
-                   spawnedTile = Instantiate(stationPrefab, new Vector3(x, y), Quaternion.identity);
-               }
-               else if (dStationTiles.Contains(tileLocation))
-               {
-                   spawnedTile = Instantiate(stationPrefab, new Vector3(x, y), Quaternion.Euler(0f, 0f, 180f));
-               }
-               else if (lStationTiles.Contains(tileLocation))
-               {
-                   spawnedTile = Instantiate(stationPrefab, new Vector3(x, y), Quaternion.Euler(0f, 0f, 90f));
-               }
-               else if (rStationTiles.Contains(tileLocation))
-               {
-                   spawnedTile = Instantiate(stationPrefab, new Vector3(x, y), Quaternion.Euler(0f, 0f, 270f));
-               }
-               else
-               {
-                   Sprite randomSprite = floorSprites[Random.Range(0, floorSprites.Length)];
-                   spawnedTile = Instantiate(floorPrefab, new Vector3(x, y), Quaternion.identity);
-                   spawnedTile.GetComponent<SpriteRenderer>().sprite = randomSprite;
-                   floorTiles.Add(tileLocation);
-               }
-
-
-               if (spawnedTile != null)
-               {
-                   spawnedTile.name = $"Tile {x} {y}";
-                   tiles[new Vector2(x, y)] = spawnedTile;
-                   spawnedTile.gridLocation = tileLocation;
-               }
-           }
-       }
-       //cam.transform.position = new Vector3((float)width / 2 - 0.5f, (float)height / 2 - 0.5f, -10);
-   }
-
-   public void Patched(Tile tile)
+    public void Patched(Tile tile)
     {
         tile.GetComponent<SpriteRenderer>().sprite = floorSprites[Random.Range(0, floorSprites.Length)];
         tile.tag = "Floor";
-    } 
+
+    }
+
+    void GenerateBreach()
+    {
+        if (floorTiles.Count > 0)
+        {
+            Vector2Int breachLocation = floorTiles[Random.Range(0, floorTiles.Count)];
+
+            if (tiles.TryGetValue(breachLocation, out Tile breachTile))
+            {
+                breachTile.GetComponent<SpriteRenderer>().sprite = breachSprite;
+                breachTile.tag = "Breach";
+                breachTiles.Add(breachLocation);  
+                StartCoroutine(IncreasePressure(breachLocation));
+            }
+        }
+    }
+
+    IEnumerator IncreasePressure(Vector2Int breachLocation)
+    {
+        while (tiles.TryGetValue(breachLocation, out Tile breachTile) && breachTile.tag == "Breach")
+        {
+            PressureSpread(breachLocation);  
+            yield return null;  
+        }
+    }
 
 
-   void GenerateBreach()
-   {
-       if (floorTiles.Count > 0)
-       {
-           Vector2Int breachLocation = floorTiles[Random.Range(0, floorTiles.Count)];
+    void PressureSpread(Vector2Int breachLocation)
+    {
+        Queue<Vector2Int> tilesToCheck = new Queue<Vector2Int>();
 
-           if (tiles.TryGetValue(breachLocation, out Tile breachTile))
-           {
-               breachTile.GetComponent<SpriteRenderer>().sprite = breachSprite;
-               breachTile.tag = "Breach";
-           }
-       }
-   }
+        List<Vector2Int> affectedTiles = new List<Vector2Int>();
+        affectedTiles.Add(breachLocation);
+        tilesToCheck.Enqueue(breachLocation);
+
+        while (tilesToCheck.Count > 0)
+        {
+            Vector2Int currentTile = tilesToCheck.Dequeue();
+
+            Vector2Int[] directions = { Vector2Int.up, Vector2Int.down, Vector2Int.left, Vector2Int.right };
+
+            foreach (var direction in directions)
+            {
+                Vector2Int neighborLocation = currentTile + direction;
+
+                if (tiles.TryGetValue(neighborLocation, out Tile neighborTile) && neighborTile.open)
+                {
+                    if (!affectedTiles.Contains(neighborLocation))
+                    {
+                        affectedTiles.Add(neighborLocation);
+                        tilesToCheck.Enqueue(neighborLocation);
+                    }
+                }
+            }
+        }
+
+        foreach(Vector2Int tileLocation in affectedTiles)
+        {
+            if (tiles.TryGetValue(tileLocation, out Tile tile))
+            {
+                tile.IncreasePressure(Time.deltaTime / affectedTiles.Count);
+            }
+        }
+    }
 }
-
-
-
